8f97b57352ceab5ed5397c563b2a6bb4998a57e5
sulihua <l1156832448@qq.com>
2020-06-02 11:39:10 +0800

    Updates

---
+++ precache-manifest.d6f80550a84686470d56a4a9015ec765.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "738600fc3d14811d37e6c7cdd8463f86",
+    "url": "/hjzf-mobile/index.html"
+  },
+  {
+    "revision": "12579d04a9941d26ea5b",
+    "url": "/hjzf-mobile/static/css/2.ec297de0.chunk.css"
+  },
+  {
+    "revision": "db8e0630ff7821cb378f",
+    "url": "/hjzf-mobile/static/css/main.6a3163c4.chunk.css"
+  },
+  {
+    "revision": "12579d04a9941d26ea5b",
+    "url": "/hjzf-mobile/static/js/2.95aeee09.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/hjzf-mobile/static/js/2.95aeee09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db8e0630ff7821cb378f",
+    "url": "/hjzf-mobile/static/js/main.b6844918.chunk.js"
+  },
+  {
+    "revision": "175077c674f2ac48eedf",
+    "url": "/hjzf-mobile/static/js/runtime-main.238cf825.js"
+  },
+  {
+    "revision": "3b538f76f922de087df0eb82f703119a",
+    "url": "/hjzf-mobile/static/media/iconfont.3b538f76.ttf"
+  },
+  {
+    "revision": "80483e3b898177297ad8b08c1e002f55",
+    "url": "/hjzf-mobile/static/media/iconfont.80483e3b.eot"
+  },
+  {
+    "revision": "c931e1ae5f45a521413c9905641f2071",
+    "url": "/hjzf-mobile/static/media/iconfont.c931e1ae.svg"
+  },
+  {
+    "revision": "f58b610e9d4e6470d644d7d195bbfda6",
+    "url": "/hjzf-mobile/static/media/iconfont.f58b610e.woff"
+  }
+]);
