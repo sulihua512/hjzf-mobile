da7208975dbd28334cecc612e272247c616b3765
sulihua <l1156832448@qq.com>
2020-06-02 09:50:37 +0800

    城市选择-左侧

--- src/pages/CityList/index.js
+++ src/pages/CityList/index.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react'
 import { getCityList, getHotCity } from '../../utils/api/city'
 import { getCurCity } from '../../utils/index';
+// 列表组件
+import { AutoSizer, List } from 'react-virtualized';
+// 引入组件样式
+import './index.scss'
+import { NavBar, Icon, Toast } from 'antd-mobile';
+import { setLocalData, CURR_CITY } from '../../utils/index'
 
+// 假数据
+// const list = Array.from(new Array(100)).map((item, index) => index)
 /* 
     城市列表
 */
 class CityList extends Component {
+    // 定义状态数据
     state = {
-        cityList: {}
+        // 归类的数据
+        cityList: {},
+        // 列表归类的分类
+        cityIndex: []
     }
     componentDidMount() {
         this.getCityData()
@@ -16,7 +28,7 @@ class CityList extends Component {
     getCityData = async () => {
         const { status, data } = await getCityList()
         if (status === 200) {
-            console.log('所有城市数据：', data);
+            // console.log('所有城市数据：', data);
             const { cityList,
                 cityIndex } = this.formatCities(data);
             // console.log('处理完的数据：', cityList, cityIndex)
@@ -33,6 +45,12 @@ class CityList extends Component {
             cityList['#'] = [curCity]
             cityIndex.unshift('#')
             console.log(cityList);
+            // 做响应式处理
+            this.setState({
+                cityList,
+                cityIndex
+            })
+
         }
     }
 
@@ -68,10 +86,94 @@ class CityList extends Component {
             cityIndex
         }
     }
+    // 格式化title
+    formTitle = (title) => {
+        switch (title) {
+            case '#':
+                return '当前城市';
+            case 'hot':
+                return '热门城市';
+            default:
+                return title.toUpperCase()
+
+        }
+    }
+    // 动态计算row的高度
+    /* 
+        index 当前行的索引
+    */
+    execHeight = ({ index }) => {
+        // 获取归类的数据，并渲染列表
+        const { cityIndex, cityList } = this.state;
+        // 列表下归类：title
+        const title = cityIndex[index]
+        // 对应title下的城市列表
+        const titleCity = cityList[title]
+        return 36 + 50 * titleCity.length
+    }
+    // 切换定位城市事件函数
+    selCity = (item) => {
+        // console.log(item)
+        const hasData = ['北京', '上海', '广州', '深圳'];
+        if (hasData.includes(item.label)) {
+            // 存在
+            // 更新本地定位数据
+            setLocalData(CURR_CITY, JSON.stringify(item))
+            // 返回首页
+            this.props.history.goBack()
+        } else {
+            // 不存在
+            Toast('当前城市还暂无房源信息！！！', 2)
+        }
+    }
+    // 行渲染
+    rowRenderer = ({
+        key, // Unique key within array of rows
+        index, // Index of row within collection
+        isScrolling, // The List is currently being scrolled
+        isVisible, // This row is visible within the List (eg it is not an overscanned row)
+        style, // Style object to be applied to row (to position it)
+    }) => {
+        // 获取归类的数据，并渲染列表
+        const { cityIndex, cityList } = this.state;
+        // 列表下归类：title
+        const title = cityIndex[index]
+        // 对应title下的城市列表
+        const titleCity = cityList[title]
+        return (
+            <div key={key} style={style} className="city-item">
+                <div className="title">{this.formTitle(title)}</div>
+                {titleCity.map((item) =>
+                    <div className="name" key={item.value} onClick={() => {
+                        this.selCity(item)
+                    }}>{item.label}</div>
+                )}
+            </div>
+        );
+    }
+
     render() {
         return (
-            <div>
-                CITYLIST
+            <div className="cityListBox">
+                <NavBar
+                    mode="dark"
+                    icon={<Icon type="left" />}
+                    onLeftClick={() => this.props.history.goBack()}
+                >城市列表</NavBar>
+                <AutoSizer>
+                    {({ height, width }) => (
+                        <List
+                            className="listBox"
+                            height={height}
+                            rowCount={this.state.cityIndex.length}
+                            rowHeight={this.execHeight}
+                            rowRenderer={this.rowRenderer}
+                            width={width}
+                        />
+                    )}
+                </AutoSizer>
+
+
             </div>
         )
     }
--- src/utils/index.js
+++ src/utils/index.js
@@ -47,3 +47,4 @@ export function getCurCity() {
         return Promise.resolve(currcity)
     }
 }
+export { CURR_CITY }
