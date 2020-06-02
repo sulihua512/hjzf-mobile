d828deedd0acf60dd6351fba08eeff5cd1207a05
sulihua <l1156832448@qq.com>
2020-06-02 20:35:18 +0800

    列表找房-FilterPicker

--- src/pages/House/components/Filter/index.js
+++ src/pages/House/components/Filter/index.js
@@ -17,27 +17,47 @@ const titleSelectedStatus = {
 export default class Filter extends Component {
   // 定义状态数据
   state = {
-    titleSelectedStatus: { ...titleSelectedStatus }
+    // 过滤器title选中状态
+    titleSelectedStatus: { ...titleSelectedStatus },
+    openType: ''
   }
 
   // 父组件提供修改状态数据的方法
   onTitleClick = (type) => {
     this.setState({
-      titleSelectedStatus: { ...titleSelectedStatus, [type]: true }
+      titleSelectedStatus: { ...titleSelectedStatus, [type]: true },
+      openType: type
+    })
+  }
+  // 控制前三个过滤器内容显示
+  isShowPicker = () => {
+    const { openType } = this.state;
+    return openType === 'area' || openType === 'mode' || openType === 'price'
+  }
+  // 确定的时候=》关闭前三个菜单的内容
+  onOk = () => {
+    this.setState({
+      openType: ''
+    })
+  }
+  // 取消的时候=》关闭前三个菜单的内容
+  onCancel = () => {
+    this.setState({
+      openType: ''
     })
   }
   render() {
     return (
       <div className={styles.root}>
         {/* 前三个菜单的遮罩层 */}
-        {/* <div className={styles.mask} /> */}
+        {this.isShowPicker() ? <div className={styles.mask} onClick={this.onCancel} /> : null}
 
         <div className={styles.content}>
           {/* 标题栏 */}
           <FilterTitle titleSelectedStatus={this.state.titleSelectedStatus} onTitleClick={this.onTitleClick} />
 
           {/* 前三个菜单对应的内容： */}
-          {/* <FilterPicker /> */}
+          {this.isShowPicker() ? <FilterPicker onCancel={this.onCancel} onOk={this.onOk} /> : null}
 
           {/* 最后一个菜单对应的内容： */}
           {/* <FilterMore /> */}
